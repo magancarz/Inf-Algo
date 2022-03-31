a20c392502f97b52046d458d02b4f03be2cb6aa1
magancarz <grzmateusz6@gmail.com>
2022-03-31 23:58:46 +0200

    Lista 2 KiKD poprawka

--- KiKD/Lista 2/kkdlista2.cpp
+++ KiKD/Lista 2/kkdlista2.cpp
@@ -8,9 +8,9 @@
 #include <cmath>
 #include <iomanip>
 
+// variables
 unsigned int initialFileSize = 0;
 unsigned int finalFileSize = 0;
-double compressionRatio = 0;
 double entropy = 0;
 
 double calculate_entropy(std::ifstream* file) {
@@ -21,6 +21,7 @@ double calculate_entropy(std::ifstream* file) {
 	while (!file -> eof()) {
 	    characters[letter]++;
 
+	    // get the next character
 	    letter = file -> get();
 
 	    size++;
@@ -39,14 +40,12 @@ double calculate_entropy(std::ifstream* file) {
 	return log2lsize - sum_divided_by_size;
 }
 
-// A Tree node
 struct Node {
 	char ch;
 	int freq;
 	Node *left, *right;
 };
 
-// Function to allocate a new tree node
 Node* getNode(char ch, int freq, Node* left, Node* right) {
 	Node* node = new Node();
 
@@ -58,22 +57,17 @@ Node* getNode(char ch, int freq, Node* left, Node* right) {
 	return node;
 }
 
-// Comparison object to be used to order the heap
 struct comp {
 	bool operator()(Node* l, Node* r) {
-		// highest priority item has lowest frequency
 		return l->freq > r->freq;
 	}
 };
 
-// traverse the Huffman Tree and store Huffman Codes
-// in a map.
 void encode(Node* root, std::string str,
 			std::unordered_map<char, std::string> &huffmanCode) {
 	if (root == nullptr)
 		return;
 
-	// found a leaf node
 	if (!root->left && !root->right) {
 		huffmanCode[root->ch] = str;
 	}
@@ -82,13 +76,11 @@ void encode(Node* root, std::string str,
 	encode(root->right, str + "1", huffmanCode);
 }
 
-// traverse the Huffman Tree and decode the encoded string
 void decode(Node* root, int &index, std::string str) {
 	if (root == nullptr) {
 		return;
 	}
 
-	// found a leaf node
 	if (!root->left && !root->right) {
 		std::cout << root->ch;
 		return;
@@ -102,45 +94,28 @@ void decode(Node* root, int &index, std::string str) {
 		decode(root->right, index, str);
 }
 
-// Builds Huffman Tree and decode given input text
 void buildHuffmanTree(std::string text) {
-	// count frequency of appearance of each character
-	// and store it in a map
 	std::unordered_map<char, int> freq;
 	for (char ch: text) {
 		freq[ch]++;
 	}
 
-	// Create a priority queue to store live nodes of
-	// Huffman tree;
 	std::priority_queue<Node*, std::vector<Node*>, comp> pq;
 
-	// Create a leaf node for each character and add it
-	// to the priority queue.
 	for (auto pair: freq) {
 		pq.push(getNode(pair.first, pair.second, nullptr, nullptr));
 	}
 
-	// do till there is more than one node in the queue
 	while (pq.size() != 1) {
-		// Remove the two nodes of highest priority
-		// (lowest frequency) from the queue
 		Node *left = pq.top(); pq.pop();
 		Node *right = pq.top();	pq.pop();
 
-		// Create a new internal node with these two nodes
-		// as children and with frequency equal to the sum
-		// of the two nodes' frequencies. Add the new node
-		// to the priority queue.
 		int sum = left->freq + right->freq;
 		pq.push(getNode('\0', sum, left, right));
 	}
 
-	// root stores pointer to root of Huffman Tree
 	Node* root = pq.top();
 
-	// traverse the Huffman Tree and store Huffman Codes
-	// in a map. Also prints them
 	std::unordered_map<char, std::string> huffmanCode;
 	encode(root, "", huffmanCode);
 
@@ -151,7 +126,6 @@ void buildHuffmanTree(std::string text) {
 
 	std::cout << "\nOriginal string was :\n" << text << '\n';
 
-	// print encoded string
 	std::string str = "";
 	for (char ch: text) {
 		str += huffmanCode[ch];
@@ -159,12 +133,10 @@ void buildHuffmanTree(std::string text) {
 
 	std::cout << "\nEncoded string is :\n" << str << '\n';
 	finalFileSize = str.size();
-	compressionRatio = finalFileSize / initialFileSize;
+	std::cout << "Compression ratio: " << finalFileSize / initialFileSize << "%\n";
 	
 	std::cout << "Entropy: " << entropy << std::endl;
 
-	// traverse the Huffman Tree again and this time
-	// decode the encoded string
 	int index = -1;
 	std::cout << "\nDecoded string is: \n";
 	while (index < (int)str.size() - 2) {
@@ -179,7 +151,6 @@ void buildHuffmanTree(std::string text) {
 	std::cout << "Saved to the file.\n";
 }
 
-// Huffman coding algorithm
 int main(int argc, char* argv[]) {
 /*
 	if(argc < 2) {
@@ -195,7 +166,7 @@ int main(int argc, char* argv[]) {
 
 	//double entropy = calculate_entropy(&file);
 
-	std::ifstream file("PanTadeusz");
+	std::ifstream file("Kordian");
 	//std::ifstream file(argv[1], std::ios::in | std::ios::binary);
 	//entropy = calculate_entropy(&t);
 	std::stringstream buffer;
@@ -207,8 +178,6 @@ int main(int argc, char* argv[]) {
 	buildHuffmanTree(text);
 	auto end = std::chrono::high_resolution_clock::now();
 	
-	
-	std::cout << "Compression ratio: " << compressionRatio << "%\n";
 	std::cout << std::setprecision(15) << "Huffman coding time: " <<  std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / 1000000000.0 << std::endl;
 
 	return 0;
