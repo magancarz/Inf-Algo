27b7462527c0ce2bc4b6b09c00c1af5004cd7825
magancarz <grzmateusz6@gmail.com>
2023-03-09 12:46:13 +0100

    AM Lista 1: aplikacja z wykładu kotlin

---
+++ AOD/Lista1/main.cpp
@@ -0,0 +1,22 @@
+#include "src/graph/Graph.h"
+
+#include <iostream>
+
+void runTests() {
+	for (const auto& i : { 1, 2, 3, 4, 5, 6 }) {
+		Graph g;
+		std::string filepath = "tests/2/g2a-" + std::to_string(i) + ".txt";
+		g.loadDataFromFileToGraph(filepath);
+		std::cout << "Running topological sort on " << filepath << std::endl;
+		g.topologicalSort();
+
+		filepath = "tests/2/g2b-" + std::to_string(i) + ".txt";
+		g.loadDataFromFileToGraph(filepath);
+		std::cout << "Running topological sort on " << filepath << std::endl;
+		g.topologicalSort();
+	}
+}
+
+int main() {
+	runTests();
+}
