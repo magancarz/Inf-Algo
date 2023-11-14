03cd4de197fd1509fb0c05d7a06b3980757e4a9e
magancarz <grzmateusz6@gmail.com>
2023-11-14 19:27:04 +0100

    wip

--- PW/Lista1/ex1.go
+++ PW/Lista1/ex1.go
@@ -7,8 +7,8 @@ import (
 )
 
 const (
-    m        = 5
-    n        = 5
+    m        = 8
+    n        = 8
     k        = 5
     maxMoves = 10
 )
@@ -16,10 +16,18 @@ const (
 type traveler struct {
     id   int
     x, y int
+    is_alive bool
+}
+
+type wild_locator struct {
+    d    time.Duration
+    x, y int
+    is_alive bool
 }
 
 type Request struct {
     t *traveler
+    wl *wild_locator
     move [2]int
     leave bool
 }
@@ -30,7 +38,9 @@ type Response struct {
 
 type node struct {
     traveler *traveler
+    wild_locator * wild_locator
     occupied bool
+    mark rune
     x, y int
     from_below int
     from_side  int
@@ -45,10 +55,10 @@ func (t *traveler) move(grid *[][]node) {
         var newX, newY = t.x + move[0], t.y + move[1]
         var oldX, oldY = t.x, t.y
         if newX >= 0 && newX < m && newY >= 0 && newY < n {
-            (*grid)[newY][newX].requests <- Request{t, move, false}
+            (*grid)[newY][newX].requests <- Request{t, nil, move, false}
             var response = <- (*grid)[newY][newX].responses
             if response.allowed {
-                (*grid)[oldY][oldX].requests <- Request{t, move, true}
+                (*grid)[oldY][oldX].requests <- Request{t, nil, move, true}
                 <- (*grid)[oldY][oldX].responses
                 return
             }
@@ -76,13 +86,24 @@ func server(node *node) {
             if node.occupied {
                 (*node).responses <- Response{allowed: false}
             } else {
-                (*node).occupied = true
-                (*node).traveler = request.t
+                var oldX, oldY = 0, 0
+                if request.wl != nil {
+                    (*node).wild_locator = request.wl
+                    (*node).mark = '*'
 
-                var oldX, oldY = (*node).traveler.x, (*node).traveler.y
-                (*node).traveler.x = node.x
-                (*node).traveler.y = node.y
+                    oldX, oldY = (*node).traveler.x, (*node).traveler.y
 
+                    (*node).wild_locator.x = node.x
+                    (*node).wild_locator.y = node.y
+                } else if request.t != nil {
+                    (*node).traveler = request.t
+                    (*node).mark = request.t.id
+
+                    oldX, oldY = (*node).traveler.x, (*node).traveler.y
+
+                    (*node).traveler.x = node.x
+                    (*node).traveler.y = node.y
+                }
                 if request.move[0] != 0 {
                     if (node.x < oldX) {
                         (*node).from_side = 1
@@ -92,6 +113,7 @@ func server(node *node) {
                         (*node).from_below = 1
                     }
                 }
+                (*node).occupied = true
                 (*node).responses <- Response{allowed: true}
             }
         }
@@ -112,7 +134,7 @@ func spawnTraveler(travelers *[]traveler, grid *[][]node, next_free_id *int) {
     for {
         (*t).x = rand.Intn(m)
         (*t).y = rand.Intn(n)
-        (*grid)[t.y][t.x].requests <- Request{t, [2]int{0, 0}, false}
+        (*grid)[t.y][t.x].requests <- Request{t, nil, [2]int{0, 0}, false}
         var response = <- (*grid)[t.y][t.x].responses
         if response.allowed {
             break
@@ -128,8 +150,49 @@ func spawnTravelers(travelers *[]traveler, grid *[][]node, next_free_id *int) {
         if (*next_free_id) < (m * n - 1) && rand.Intn(100) < 30 {
             spawnTraveler(travelers, grid, next_free_id)
         }
-        time.Sleep(time.Duration(100 * time.Millisecond))
+        time.Sleep(time.Duration(rand.Intn(10)) * 100 * time.Millisecond)
     }
+}
+
+func spawnWildLocators(wild_locators *[]wild_locator, grid *[][]node) {
+    for {
+        if rand.Intn(100) < 30 {
+            index = -1
+            for i := 0; i < wild_locators.len; i++ {
+                if !wild_locators[i].is_alive {
+                    index = i
+                    break
+                }
+            }
+            if index == -1 {
+                continue
+            }
+            spawnWildLocator(wild_locators, index, grid)
+        }
+        time.Sleep(time.Duration(rand.Intn(10)) * 100 * time.Millisecond)
+    }
+}
+
+func spawnWildLocator(wild_locators *[]wild_locator, index int, grid *[][]node) {
+    var x, y int
+    (*wild_locators)[index] = wild_locator{time.Duration(rand.Intn(10)) * 100 * time.Millisecond, x: x, y: y, is_alive: true}
+    var wl  = &(*wild_locators)[index]
+    for {
+        (*wl).x = rand.Intn(m)
+        (*wl).y = rand.Intn(n)
+        (*grid)[wl.y][wl.x].requests <- Request{wl, [2]int{0, 0}, false, true}
+        var response = <- (*grid)[wl.y][wl.x].responses
+        if response.allowed {
+            break
+        }
+    }
+    go wildLocatorLifetime(wl, index, grid)
+}
+
+func wildLocatorLifetime(wild_locator *wild_locator, index int, grid *[][]node) {
+    time.Sleep(wild_locator.d)
+    (*grid)[wild_locator.y][wild_locator.x].requests <- Request{nil, wild_locator, [2]int{0, 0}, false, true}
+    var response = <- (*grid)[t.y][t.x].responses
 }
 
 func snap(move int, grid *[][]node) {
@@ -139,7 +202,7 @@ func snap(move int, grid *[][]node) {
         for j := 0; j < n; j++ {
             fmt.Print("\x1b[0m")
             if (*grid)[i][j].occupied {
-                fmt.Printf("%2d", (*grid)[i][j].traveler.id)
+                fmt.Printf("%2d", (*grid)[i][j].mark)
             } else {
                 fmt.Print("  ")
             }
@@ -202,16 +265,19 @@ func main() {
     var next_free_id = 0
     travelers := make([]traveler, (m * n) - 1)
     for i := 0; i < k; i++ {
-        spawnTraveler(&travelers, &grid, &next_free_id)
+        // spawnTraveler(&travelers, &grid, &next_free_id)
     }
 
+    wild_locators := make([]wild_locator, (m * n) - 1)
+
     for i := 0; i < k; i++ {
-        go travelerMovement(&travelers[i], &grid, maxMoves)
+        // go travelerMovement(&travelers[i], &grid, maxMoves)
     }
 
     go snapshot(&grid, maxMoves)
 
-    go spawnTravelers(&travelers, &grid, &next_free_id)
+    // go spawnTravelers(&travelers, &grid, &next_free_id)
+    go spawnWildLocators(&wild_locators, &grid)
 
-    time.Sleep(1 * time.Second)
+    time.Sleep(5 * time.Second)
 }
